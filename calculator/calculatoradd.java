a20f1274028d96fc55c7b873167603f00ef6dacb
Bhumi-gayakwad <bhumigayakwad61@gmail.com>
2025-01-07 12:10:15 +0530

    add calculator.java

---
+++ calculator/calculatoradd.java
@@ -0,0 +1,9 @@
+  import java.util.*;
+  public class caladd{
+public static void main(string[] args){
+ Scanner sc=new Scanner(System.in);
+ int x=sc.nextInt();
+ int y=sc.nextInt();
+ System.out.println(x+y);
+ }
+ }
