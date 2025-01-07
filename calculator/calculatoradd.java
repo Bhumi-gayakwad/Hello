5cbf7c3f30c84f691daf9341f7edb3ce4c88a639
Bhumi-gayakwad <bhumigayakwad61@gmail.com>
2025-01-07 12:23:38 +0530

    add calculator

--- calculator/calculatoradd.java
+++ calculator/calculatoradd.java
@@ -1,9 +1,16 @@
   import java.util.*;
-  public class caladd{
-public static void main(string[] args){
+  public class calculator{
+public static void main(String[] args){
  Scanner sc=new Scanner(System.in);
  int x=sc.nextInt();
- int y=sc.nextInt();
- System.out.println(x+y);
- }
+ int y=sc.nextInt(); 
+ int sum=x+y;
+ System.out.println(sum);
+ int substract=x-y;
+ System.out.println(substract);
+ int multiplication=x*y;
+ System.out.println(multiplication);
+ int division=x/y;
+ System.out.println(division);
+  }
  }
